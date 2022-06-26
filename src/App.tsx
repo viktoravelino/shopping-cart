c3ff1ab6bad750343bb6ad05e7ce93bba24ff5ac
Viktor Avelino <viktor.avelino@gmail.com>
2022-06-26 15:11:11 -0400

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+
+import { Navbar } from './components/Navbar';
+
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="store" element={<Store />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="shopping-cart">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
