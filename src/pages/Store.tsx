335dc312926691580ce1538662dc48bf8f7e8f2c
Viktor Avelino <viktor.avelino@gmail.com>
2022-06-26 15:21:18 -0400

    card title and price

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,19 @@
+import { Row, Col } from 'react-bootstrap';
+import { StoreItem } from '../components/StoreItem';
+import storeItems from '../data/items.json';
+
 export function Store() {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
