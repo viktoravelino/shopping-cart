ed35a6e60edecd25e994b4b6ee16435f7bd12aae
Viktor Avelino <viktor.avelino@gmail.com>
2022-06-30 09:31:16 -0400

    update domain

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="shopping-cart">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
